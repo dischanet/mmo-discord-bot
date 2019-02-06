d85a751ec1684f4a891c4f11950d1f6894a66eb3
Yusuke Hayashi <yusuke8h@gmail.com>
2019-02-06 21:38:42 +0900

    デプロイ用設定を更新

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
 cd /home/ec2-user/repos/mmo-discord-bot
-
 /usr/bin/npm install --production
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]
-then
-    PARAMETER_NAME=MMO_DEVELOP_DISCORD_BOT_SECRET
-else
-    PARAMETER_NAME=MMO_DISCORD_BOT_SECRET
-fi
+PARAMETER_NAME=MMO_DISCORD_BOT_SECRET
 REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameter --name ${PARAMETER_NAME} --query "Parameter.Value" --output text)" > environment
 
