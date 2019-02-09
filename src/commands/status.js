b2426149bad50a908ef4dea86dc30be3be8bfbbc
Yusuke Hayashi <yusuke8h@gmail.com>
2019-02-09 22:18:35 +0900

    embedに変更

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,46 +1,61 @@
-module.exports = async (client, message, db) => {
-  const userId = message.author.id;
-  const playerExp = await db.getPlayerExp(userId);
-  const playerLevel = Math.round(Math.sqrt(playerExp));
-  const rank = await db.getRank(userId);
+const getItemMessage = async (db, userId) => {
+  let result = "";
+  const playerItems = await db.getPlayerItems(userId);
+  if (playerItems.includes(-10)) result += "【運営の証】を持っている。\n";
+  if (playerItems.includes(-9)) result += "【サポーターの証】を持っている。\n";
+  return result;
+};
 
-  let itemComment = "";
-  const playerItems = await db.getItems(userId);
-  if (playerItems.includes(-10)) itemComment += "【運営の証】を持っている。\n";
-  if (playerItems.includes(-9))
-    itemComment += "【サポーターの証】を持っている。\n";
-
-  let statusComment = `<@${userId}>のステータス
-Lv: ${playerLevel}
-HP: ${playerLevel * 5 + 50}
-攻撃力: ${playerLevel * 2 + 10}
-EXP: ${playerExp}
-次のレベルまで ${(playerLevel + 1) ** 2 - playerExp}exp
-${itemComment}
-プレイヤーランクは${rank}位だ！`;
-
+const getButtleData = async (client, db, userId, maxHp) => {
+  let currentHp = maxHp;
+  let battleField = "-";
   const PlayerInBattle = await db.getPlayerBattle(userId);
   if (PlayerInBattle) {
     const battleChannel = client.channels.get(PlayerInBattle.channelId);
-    if (battleChannel) {
-      const battleField = battleChannel.name
+    if (!battleChannel) {
+      // await deleteInBattle(PlayerInBattle.channelId);
+    } else {
+      currentHp = PlayerInBattle.playerHp;
+      battleField = battleChannel.name
         ? `${battleChannel.guild.name}の#${battleChannel.name}`
         : "個人チャット";
-
-      statusComment = `<@${userId}>のステータス
-Lv: ${playerLevel}
-HP: ${PlayerInBattle.playerHp} / ${playerLevel * 5 + 50}
-攻撃力: ${playerLevel * 2 + 10}
-EXP: ${playerExp}
-次のレベルまで ${(playerLevel + 1) ** 2 - playerExp}exp
-
-${battleField}で戦闘中！
-${itemComment}
-プレイヤーランクは${rank}位だ！`;
-    } else {
-      // await deleteInBattle(PlayerInBattle.channelId);
     }
   }
-
-  message.channel.send(statusComment);
+  return { currentHp, battleField };
 };
+
+const statusMessage = async (client, message, db) => {
+  const userId = message.author.id;
+  const playerExp = await db.getPlayerExp(userId);
+  const playerLevel = Math.round(Math.sqrt(playerExp));
+  const rank = await db.getPlayerRank(userId);
+  const itemMessage = getItemMessage(db, userId);
+  const maxHp = playerLevel * 5 + 50;
+  const { currentHp, battleField } = getButtleData(client, db, userId, maxHp);
+
+  return {
+    embed: {
+      title: `<@${userId}> のステータス`,
+      url: message.author.avatarURL,
+      color: 7506394,
+      timestamp: new Date(),
+      thumbnail: { url: message.author.avatarURL },
+      fields: [
+        { name: "Lv:", value: playerLevel, inline: true },
+        { name: "HP:", value: `${currentHp}/${maxHp}`, inline: true },
+        { name: "攻撃力:", value: playerLevel * 2 + 10, inline: true },
+        { name: "所持アイテム:", value: itemMessage, inline: true },
+        { name: "戦闘場所:", value: battleField, inline: true },
+        {
+          name: "次のレベルまで",
+          value: `${(playerLevel + 1) ** 2 - playerExp}exp`,
+          inline: true
+        },
+        { name: "プレイヤーランク", value: `${rank}位だ！`, inline: true }
+      ]
+    }
+  };
+};
+
+module.exports = async (client, message, db) =>
+  message.channel.send(statusMessage(client, message, db));
