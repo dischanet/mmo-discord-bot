72d8ec0c09ffcc329946781e8c941674c9f79e87
hal-shu-sato <hal_shu_sato@yahoo.co.jp>
2019-02-09 13:11:07 +0900

    [機能追加] helpコマンドを追加

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 module.exports = (client, message, db) => {
-  message.channel.send("工事中です。");
+  const helpComment = `\`\`\`MMOくんはみんなでボスを倒して行くRPGです。
+
+help    このメッセージを表示します
+attack  攻撃する
+status  自分のステータスを確認する
+inquiry チャンネルのバトルの状態を確認する
+reset   戦いをやり直す
+t       四字熟語トレーニングをする
+q       クイズトレーニングをする
+item    アイテムを使う
+ranking 上位10サーバーのランキングを表示する\`\`\``;
+  message.channel.send(helpComment);
 };
