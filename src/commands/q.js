74449a7bde76f536fd26537ec9869dd3d95de59f
Yusuke Hayashi <yusuke8h@gmail.com>
2019-02-11 19:37:03 +0900

    qのバグを修正

--- src/commands/q.js
+++ src/commands/q.js
@@ -22,7 +22,7 @@ const getQuiz = async () => {
     2. ${quizList[1]}
     3. ${quizList[2]}
     4. ${quizList[3]}`,
-    answer: quiz.ans1
+    answer: quiz.ans1[0]
   };
 };
 
@@ -30,14 +30,14 @@ module.exports = async (client, message, db) => {
   const { quiz, answer } = await getQuiz();
   message.channel.send(quiz);
   const user = message.author;
-  const exp = Math.ceil(db.getPlayerLevel(user.id) / 10);
+  const exp = Math.ceil((await db.getPlayerLevel(user.id)) / 10);
   const filter = m => m.author === user;
   message.channel
-    .awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] })
+    .awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ["time"] })
     .then(collected => {
       if (collected.first().content === answer) {
         // db.addPlayerExp(user.id, exp)
-        message.channel.send(`正解だ！${exp}の経験値を得た。`);
+        message.channel.send(`正解だ！${exp}の経験値を得た。（未実装）`);
       } else {
         message.channel.send(`残念！正解は「${answer}」だ。`);
       }
