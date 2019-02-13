291dd559d2398cbe511938410c696f24167ccddf
Yusuke Hayashi <yusuke8h@gmail.com>
2019-02-13 23:34:08 +0900

    Attackコマンドの実装 (#14)
    * WIP attackコマンド実装
    
    * WIP attackの実装
    
    * attackの実装

--- src/commands/attack.js
+++ src/commands/attack.js
@@ -1,3 +1,240 @@
-module.exports = (client, message, db) => {
-  message.channel.send("工事中です。");
+const channelsInTransaction = new Set();
+const monsters = require("../assets/monsters");
+class Battle {
+  constructor(client, db, userId, channelId) {
+    this.client = client;
+    this.db = db;
+    this.userId = userId;
+    this.channelId = channelId;
+    this.player;
+    this.boss;
+    this.startSuccess = false;
+  }
+
+  async start() {
+    this.player = { level: await this.db.getPlayerLevel(this.userId) };
+
+    const { bossLevel, bossHp } = await this.db.getBoss(this.channelId);
+    this.boss = monsters[bossLevel % 50];
+    this.boss.level = bossLevel;
+    this.boss.hp = bossHp;
+
+    if (this.player.level / this.boss.level > 100) {
+      this.reply = "レベルが高すぎて攻撃できない！";
+      return;
+    }
+
+    if (channelsInTransaction.has(this.channelId)) {
+      this.reply = "攻撃失敗。ゆっくりコマンドを打ってね。";
+      return;
+    }
+    channelsInTransaction.add(this.channelId);
+
+    const playerBattle = await this.db.getPlayerBattle(this.userId);
+    if (!playerBattle) {
+      await this.joinBattle();
+      this.startSuccess = true;
+      return;
+    }
+
+    const battleChannel = this.client.channels.get(playerBattle.channelId);
+    // In case the battleChannel has been deleted
+    if (!battleChannel) {
+      await this.db.removeFromChannel(playerBattle.channelId);
+      await this.joinBattle();
+      this.startSuccess = true;
+      return;
+    }
+
+    if (playerBattle.channelId !== this.channelId) {
+      const battleField =
+        battleChannel.type === "dm" || battleChannel.type === "group"
+          ? "プライベートチャンネル"
+          : `${battleChannel.guild.name}の#${battleChannel.name}。`;
+      this.reply = `<@${this.userId}>は'${battleField}'で既に戦闘中だ。`;
+      return;
+    }
+
+    if (playerBattle.playerHp === 0) {
+      this.reply = `<@${this.userId}>はもうやられている！
+（戦いをやり直すには「!!reset」だ）`;
+      return;
+    }
+
+    this.startSuccess = true;
+  }
+
+  async joinBattle() {
+    this.playerHp = this.playerLevel * 5 + 50; // player_max_hp
+    await this.db.addPlayerIntoBattle(
+      this.channelId,
+      this.userId,
+      this.playerHp
+    );
+  }
+
+  async playerAttackProcess() {
+    this.boss.hp = this.boss.hp - this.playerAttackDamage;
+    if (this.playerAttack === 0) {
+      this.reply = `<@${this.userId}>の攻撃！${
+        this.bossName
+      }にかわされてしまった...！！`;
+    } else {
+      this.reply = `<@${this.userId}>の攻撃！${this.bossName}に\`${
+        this.playerAttack
+      }\`のダメージを与えた！`;
+    }
+    if (this.win) {
+      await this.winProcess();
+      await this.resetBattle(true);
+    } else {
+      this.updateBossHp();
+    }
+  }
+
+  get playerAttackDamage() {
+    if (!this._playerAttackDamage) {
+      if (this.boss.level % monsters.length in [20, 40] && this.rand < 0.1) {
+        this._playerAttackDamage = 0;
+      } else if (
+        this.boss.level % monsters.length in [2, 7, 13, 23, 34] &&
+        this.rand < 0.05
+      ) {
+        this._playerAttackDamage = 0;
+      } else if (this.rand < 0.01) {
+        this._playerAttackDamage = 0;
+      } else if (this.boss.level % monsters.length in [3, 11, 17, 32, 41]) {
+        const plus = this.rand < 0.96 ? this.rand / 3 + 0.5 : 3;
+        this._playerAttackDamage = Math.round(this.playerLevel * plus + 10);
+      } else if (this.boss.level % 5 === 0) {
+        const plus = this.rand < 0.96 ? this.rand / 2 + 0.8 : 3;
+        this._playerAttackDamage = Math.round(this.playerLevel * plus + 10);
+      } else {
+        const plus = this.rand < 0.96 ? this.rand / 2 + 1 : 3;
+        this._playerAttackDamage = Math.round(this.playerLevel * plus + 10);
+      }
+    }
+    return this._playerAttackDamage;
+  }
+
+  async winProcess() {
+    const levelUpComments = [];
+    let members = "";
+    let fireMembers = "";
+    let elixirMembers = "";
+    let prayMembers = "";
+    const exp =
+      this.boss.level % monsters.length == 0
+        ? this.boss.level * 5
+        : this.boss.level;
+
+    (await this.db.getBattleMembers()).forEach(memberId => {
+      levelUpComments.append(this.db.experience(memberId, exp));
+      members += "<@{}> ".format(memberId);
+      const p = Math.min(
+        (0.02 * this.boss.level * this.boss.level) /
+          this.db.getPlayerLevel(memberId),
+        0.1
+      );
+      if (this.boss.level % 50 === 0 && Math.random() < p) {
+        elixirMembers += "<@{}> ".format(memberId);
+        this.db.obtainItem(memberId, 1);
+      }
+      if (Math.random() < p) {
+        this.db.obtainItem(memberId, 2);
+        fireMembers += "<@{}> ".format(memberId);
+      }
+      if (Math.random() < p * 2) {
+        prayMembers += "<@{}> ".format(memberId);
+        this.db.obtainItem(memberId, 3);
+      }
+    });
+
+    if (fireMembers) {
+      fireMembers += "は`ファイアボールの書`を手に入れた！";
+    }
+    if (elixirMembers) {
+      elixirMembers += "は`エリクサー`を手に入れた！";
+    }
+    if (prayMembers) {
+      prayMembers += "は`祈りの書`を手に入れた！";
+    }
+    const LevelUpComment = levelUpComments.join("\n");
+    return `${this.monsterName}を倒した！
+${members}は\`${exp}\`の経験値を得た。
+${LevelUpComment}
+${fireMembers}
+${prayMembers}
+${elixirMembers}
+`;
+  }
+
+  get win() {
+    return this.boss.hp <= 0;
+  }
+
+  async bossAttackProcess() {
+    this.reply += `
+- ${this.bossName}のHP:\`${this.boss.hp}\`/${this.boss.level * 10 + 50}`;
+    this.playerHp = this.playerHp - this.bossAttackDamage;
+    if (this.bossAttackDamage === 0) {
+      this.reply += `${this.monsterName}の攻撃！<@${
+        this.userId
+      }>は華麗にかわした！
+ - <@${this.userId}>のHP:\`${this.playerHp}\`/${this.playerLevel * 5 + 50}`;
+    } else if (this.playerHp <= 0) {
+      await this.db.updatePlayerHp(this.userId, 0);
+      this.reply += `${this.monsterName}の攻撃！<@${this.userId}>は\`${
+        this.bossAttackDamage
+      }\`のダメージを受けた。
+ - <@${this.userId}>のHP:\`0\`/${this.playerLevel * 5 + 50}
+ <@${this.userId}>はやられてしまった。。。`;
+    } else {
+      await this.db.updatePlayerHp(this.userId, this.playerHp);
+      this.reply += `${this.monsterName}の攻撃！<@${this.userId}>は\`${
+        this.bossAttackDamage
+      }\`のダメージを受けた。
+ - <@${this.userId}>のHP:\`${this.playerHp}\`/${this.playerLevel * 5 + 50}`;
+    }
+  }
+
+  get bossAttackDamage() {
+    if (!this._bossAttackDamage) {
+      const r = Math.random();
+      if (r < 0.01) {
+        this._bossAttackDamage = 0;
+      }
+      if (this.boss.level % 50 === 0) {
+        this._bossAttackDamage = Math.round(this.boss.level * r * 256);
+      }
+      if (this.boss.level % 50 in [37, 46, 47, 48, 49]) {
+        this._bossAttackDamage = Math.round(this.boss.level * r);
+      }
+      if (this.boss.level % 5 === 0) {
+        this._bossAttackDamage = Math.round(this.boss.level * (1 + r) * 3);
+      }
+      this._bossAttackDamage = Math.round(this.boss.level * (2 + r) + 5);
+    }
+    return this._bossAttackDamage;
+  }
+
+  end() {
+    channelsInTransaction.delete(this.channelId);
+  }
+}
+
+module.exports = async (client, message, db) => {
+  const battle = new Battle(client, db, message.author.id, message.channel.id);
+  try {
+    await battle.start();
+    if (battle.startSuccess) {
+      await battle.playerAttackProcess();
+      if (!battle.win) {
+        await battle.bossAttackProcess();
+      }
+    }
+  } finally {
+    await battle.end();
+  }
+  message.channel.send(battle.reply);
 };
--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ const util = require("util");
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("mmo.db");
 
-db.promiseAll = util.promisify(db.all).bind(db);
-db.promiseGet = util.promisify(db.get).bind(db);
-db.promiseRun = util.promisify(db.run).bind(db);
+db.promiseAll = util.promisify(db.all);
+db.promiseGet = util.promisify(db.get);
+db.promiseRun = util.promisify(db.run);
 
 db.getPlayerExp = async userId => {
   const player = await db.promiseGet(
@@ -50,7 +50,94 @@ db.getPlayerItems = async userId => {
   return items.map(item => item.item_id);
 };
 
+db.addPlayerIntoBattle = (channelId, userId, playerHp) =>
+  db.promiseRun("INSERT INTO in_battle values(?,?,?)", {
+    channelId,
+    userId,
+    playerHp
+  });
+
 db.deletePlayerFromBattle = channelId =>
   db.promiseAll("DELETE FROM in_battle WHERE channel_id=?", [channelId]);
+
+db.updatePlayerHp = (userId, playerHp) =>
+  db.promiseRun("UPDATE in_battle SET player_hp=? WHERE user_id=?", [
+    userId,
+    playerHp
+  ]);
+
+db.getMembersInBattle = async channelId => {
+  const members = await db.promiseAll(
+    "SELECT * FROM in_battle WHERE channel_id=?",
+    [channelId]
+  );
+  return members.map(m => ({
+    userId: m.user_id,
+    playerHp: m.player_hp
+  }));
+};
+
+db.inBattle = channelId =>
+  db.promiseGet("SELECT 0 FROM in_battle WHERE channel_id=?", [channelId]);
+
+db.obtainItem = async (userId, itemId) => {
+  const item = await db.promiseGet(
+    "SELECT count FROM item WHERE user_id=? and item_id=?",
+    [userId, itemId]
+  );
+  if (item.count) {
+    await db.promiseRun(
+      "UPDATE item SET count=? WHERE user_id=? and item_id=?",
+      [item.count + 1, userId, itemId]
+    );
+  } else {
+    await db.promiseRun("INSERT INTO item VALUES(?,?,1)", [userId, itemId]);
+  }
+};
+
+db.getBoss = async channelId => {
+  const channelStatus = await db.promiseGet(
+    "SELECT boss_level, boss_hp FROM channel_status WHERE channel_id=?",
+    [channelId]
+  );
+  if (channelStatus) {
+    return {
+      bossLevel: channelStatus.boss_level,
+      bossHp: channelStatus.boss_hp
+    };
+  }
+  db.promiseRun("INSERT INTO channel_status values( ?, 1, 50)", [channelId]);
+  return { bossLevel: 1, bossHp: 50 };
+};
+
+db.updateBossHp = (channelId, bossHp) =>
+  db.promiseRun("UPDATE channel_status SET boss_hp=? WHERE channel_id=?", [
+    bossHp,
+    channelId
+  ]);
+
+db.removeFromGuild = async guild => {
+  guild.channels.forEach(async channel => {
+    await db.promiseRun("DELETE FROM in_battle WHERE channel_id=?", [
+      channel.id
+    ]);
+    await db.promiseRun("DELETE FROM channel_status WHERE channel_id=?", [
+      channel.id
+    ]);
+  });
+};
+
+db.removeFromChannel = channelId =>
+  db.promiseRun("DELETE FROM in_battle WHERE channel_id=?", [channelId]);
+
+db.resetBattle = async (channelId, levelUp) => {
+  await db.promiseRun("DELETE FROM in_battle WHERE channel_id=?", [channelId]);
+  await db.promiseRun(
+    levelUp
+      ? "UPDATE channel_status SET boss_level=boss_level+1, boss_hp=boss_level*10+60 WHERE channel_id=?"
+      : "UPDATE channel_status SET boss_hp=boss_level*10+50 WHERE channel_id=?",
+    [channelId]
+  );
+};
 
 module.exports = db;
