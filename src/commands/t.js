9725b463974a1b9227f06c9c21a33619f8b8de91
hal-shu-sato <hal_shu_sato@yahoo.co.jp>
2019-03-11 18:26:40 +0900

    [修正] CamelCaseに統一

--- src/commands/t.js
+++ src/commands/t.js
@@ -1,28 +1,27 @@
-const training_set = requier("training.json");
-const dbjs = requier("../db.js");
+const training_set = require("training.json");
+const dbjs = require("../db.js");
 
 module.exports = async (client, message, db) => {
   const user = message.author;
 
-  const mmo_db = dbjs(db);
+  const mmoDb = dbjs(db);
 
-  const q_id = Math.floor(Math.random() * (619 + 1));
-  const quiz = training_set[q_id];
+  const qId = Math.floor(Math.random() * (619 + 1));
+  const quiz = training_set[qId];
   const question = quiz[0];
   const answer = quiz[1];
 
-  const exp = mmo_db.getPlayerExp(user.id) / 8;
+  const exp = mmoDb.getPlayerExp(user.id) / 8;
 
   message.channel.send(`「${question}」の読み方をひらがなで答えなさい。`);
 
-  const response_msg = await message.channel.awaitMessages(
+  const responseMsg = await message.channel.awaitMessages(
     answer_msg => user.id === answer_msg.author.id,
     { max: 1, time: 12000 }
   );
 
-  if (response_msg) {
+  if (responseMsg) {
     if (answer === resizeBy.first().content) {
-
     }
   } else {
     message.channel.send(`時間切れだ。正解は「${answer}」だ。`);
